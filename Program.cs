c49ce381440a73bf6b8b89bb7ea83f05965df034
Kamil <mentos.96@o2.pl>
2023-06-12 22:49:53 +0200

    Write clas and enums and test the program :D

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Game_Card;
+
+
+internal class Program
+{
+    public static Random random = new Random();
+    private static void Main(string[] args)
+    {
+        Card myCard = new Card((EValue)random.Next(1,14), (ESuit)random.Next(0,4));
+        Console.WriteLine(myCard.Name);
+    }
+}
